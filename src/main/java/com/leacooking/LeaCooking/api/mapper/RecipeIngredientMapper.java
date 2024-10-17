996f660aefa2759ad881bcc9ae0da793c47e89a5
a.tho <a.tho@lyon.ort.asso.fr>
2024-10-17 23:00:09 +0200

    fix: quantityType target mapping & mapstruct config

--- src/main/java/com/leacooking/LeaCooking/api/mapper/RecipeIngredientMapper.java
+++ src/main/java/com/leacooking/LeaCooking/api/mapper/RecipeIngredientMapper.java
@@ -1,15 +1,18 @@
 package com.leacooking.LeaCooking.api.mapper;
 
+import com.leacooking.LeaCooking.api.config.mapper.MapstructConfig;
 import com.leacooking.LeaCooking.api.dto.recipeingredient.RecipeIngredientDTO;
 import com.leacooking.LeaCooking.api.entity.RecipeIngredient;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(config = MapstructConfig.class,
+        uses = { RecipeMapper.class, IngredientMapper.class })
 public interface RecipeIngredientMapper {
     @Mapping(source = "ingredient.id", target = "ingredientId")
     @Mapping(source = "ingredient.label", target = "ingredientLabel")
     @Mapping(source = "ingredientQuantity", target = "quantity")
     @Mapping(source = "recipePart.label", target = "recipePartLabel")
+    @Mapping(source = "ingredient.quantityType.label", target = "quantityTypeLabel")
     RecipeIngredientDTO toDTO(RecipeIngredient recipeIngredient);
 }
