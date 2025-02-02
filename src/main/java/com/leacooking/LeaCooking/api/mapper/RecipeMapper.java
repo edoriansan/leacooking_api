cba66f29ae9f557934f9ebb846c1c641e059b3f1
Adrien THO <star52@live.fr>
2025-02-02 11:08:35 +0000

    feat: recipe part ingredients

---
+++ src/main/java/com/leacooking/LeaCooking/api/mapper/RecipeMapper.java
@@ -0,0 +1,13 @@
+package com.leacooking.LeaCooking.api.mapper;
+
+import com.leacooking.LeaCooking.api.config.mapper.MapstructConfig;
+import com.leacooking.LeaCooking.api.dto.recipe.RecipeDTO;
+import com.leacooking.LeaCooking.api.entity.Recipe;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapstructConfig.class, uses = { RecipePartMapper.class })
+public interface RecipeMapper {
+    @Mapping(source = "recipeType.label", target = "recipeType")
+    RecipeDTO toDTO(Recipe recipe);
+}
