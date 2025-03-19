f5ac06fede8d6aaedf20295c84ee3529e62410f6
edoriansan <star52@live.fr>
2025-03-19 10:57:47 +0100

    fix: db schema

--- src/main/resources/db/migration/V0__init.sql
+++ src/main/resources/db/migration/V0__init.sql
@@ -1,7 +1,6 @@
 create sequence recipe_category_seq;
 create sequence recipe_subcategory_seq;
 create sequence ingredient_seq;
-create sequence quantity_type_seq;
 create sequence recipe_part_seq;
 create sequence recipe_seq;
 create sequence recipe_part_ingredient_seq;
@@ -19,15 +18,9 @@ create table "recipe_subcategory" (
                                id_recipe_category BIGINT REFERENCES recipe_category
 );
 
-create table "quantity_type" (
-                                 id bigint default nextval('quantity_type_seq') primary key,
-                                 label VARCHAR(255)
-);
-
 create table "ingredient" (
                               id bigint default nextval('ingredient_seq') primary key,
-                              label VARCHAR(255) NOT NULL,
-                              id_quantity_type BIGINT REFERENCES quantity_type
+                              label VARCHAR(255) NOT NULL
 );
 
 create table "recipe" (
@@ -41,7 +34,7 @@ create table "recipe" (
 create table "recipe_part" (
                                id bigint default nextval('recipe_part_seq') primary key,
                                recipe_part_title VARCHAR(255),
-                               recipe_part_process VARCHAR(255) NOT NULL,
+                               recipe_part_process VARCHAR(1000) NOT NULL,
                                id_recipe BIGINT REFERENCES recipe ON DELETE CASCADE
 );
 
@@ -49,12 +42,11 @@ create table "recipe_part_ingredient" (
                                           id bigint default nextval('recipe_part_ingredient_seq') primary key,
                                           id_recipe_part BIGINT REFERENCES recipe_part ON DELETE CASCADE,
                                           id_ingredient BIGINT REFERENCES ingredient,
-                                          quantity INT NOT NULL
+                                          quantity VARCHAR(255)
 );
 
 create index index_recipe_category__recipe_subcategory on recipe_subcategory (id_recipe_category);
 create index index_recipe__recipe_subcategory on recipe (id_recipe_subcategory);
-create index index_ingredient__quantity_type on ingredient (id_quantity_type);
 create index index_recipe_part__recipe on recipe_part (id_recipe);
 create index index_recipe_part_ingredient__recipe_part on recipe_part_ingredient (id_recipe_part);
 create index index_recipe_part_ingredient__ingredient on recipe_part_ingredient (id_ingredient);
