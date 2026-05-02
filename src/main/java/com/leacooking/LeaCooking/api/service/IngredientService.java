a7680db3c65190d733af88cb9fe03f1b36720f5a
Adrien THO <star52@live.fr>
2026-05-02 20:05:22 +0200

    feat: add ingredient crud methods & endpoints

--- src/main/java/com/leacooking/LeaCooking/api/service/IngredientService.java
+++ src/main/java/com/leacooking/LeaCooking/api/service/IngredientService.java
@@ -7,7 +7,10 @@ import com.leacooking.LeaCooking.api.exception.ApiException;
 import com.leacooking.LeaCooking.api.mapper.IngredientMapper;
 import com.leacooking.LeaCooking.api.repository.IngredientRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,15 +21,39 @@ public class IngredientService {
     private final IngredientRepository ingredientRepository;
     private final IngredientMapper ingredientMapper;
 
+    public IngredientDTO saveIngredient(IngredientDTO ingredientToSave) {
+        Ingredient ingredient = ingredientRepository.save(
+                ingredientMapper.toEntity(ingredientToSave)
+        );
+        return ingredientMapper.toDTO(ingredient);
+    }
+
     public List<IngredientDTO> getAllIngredients() throws ApiException {
         List<Ingredient> ingredients = ingredientRepository.findAll();
 
         if (ingredients.isEmpty()) {
-            throw new ApiException(ErrorEnum.E500, "No ingredients found.");
+            throw new ApiException(ErrorEnum.E500, "No ingredients found");
         }
 
         return ingredients.stream()
                 .map(ingredientMapper::toDTO)
                 .collect(Collectors.toList());
+    }
+
+    public IngredientDTO updateIngredient(Long ingredientId, IngredientDTO ingredientToSave) throws ApiException {
+        Ingredient ingredient = ingredientRepository.findById(ingredientId)
+                .orElseThrow(() -> new ApiException(ErrorEnum.E404, "No ingredient not found for id : " + ingredientId));
+
+        ingredient.setLabel(ingredientToSave.getLabel());
+        Ingredient updatedIngredient = ingredientRepository.save(ingredient);
+
+        return ingredientMapper.toDTO(updatedIngredient);
+    }
+
+    public void deleteIngredientById(Long ingredientId) throws ApiException {
+        if (!ingredientRepository.existsById(ingredientId)) {
+            throw new ApiException(ErrorEnum.E404, "No ingredient not found for id : " + ingredientId);
+        }
+        ingredientRepository.deleteById(ingredientId);
     }
 }
